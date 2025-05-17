44985047dd3bb56181d8becb70812d6d83572ec7
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-17 17:23:27 +0700

    uji semua constructor mahasiswa di fungsi main

--- Constructor1.cpp
+++ Constructor1.cpp
@@ -85,5 +85,34 @@ class mahasiswa{
     int nim;
     string nama;
     public:
-    mahasiswa
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+mahasiswa::mahasiswa(){
+    nim = 0;
+    nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim){
+    nim=iNim;
+}
+
+mahasiswa::mahasiswa(string iNama){
+    nama=iNama
+}
+mahasiswa::mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+void mahasiswa::cetak(){
+    cout<<endl<<"Nim="<<nim<<endl;
+    cout<<"Nama="<<nama<<endl;
+}
+
+int main(){
+    
 }
